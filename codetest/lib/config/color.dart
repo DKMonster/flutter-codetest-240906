7ff04e12870eb2d233ccf422536ae87aedc46e5a
David <davidkross@mapswalker.com>
2024-09-07 15:04:08 +0800

    feat(launches): Add the basic text and color setting. and implement the scrollbar and appbar.

--- codetest/lib/config/color.dart
+++ codetest/lib/config/color.dart
@@ -1,1 +1,14 @@
 import 'package:flutter/material.dart';
+
+const appBarColor = Color(0xFF252525);
+const appBarTextColor = Color(0xFFf8f8f8);
+const appBarIconColor = Color(0xFFf8f8f8);
+
+const bodyColor = Color(0xFF252525);
+const bodyTextColor = Color(0xFFF5F5F5);
+const bodyIconColor = Color(0xFFF5F5F5);
+
+const subTextActionColor = Color(0xFFdcbbac);
+
+const textColor = Color(0xFFe3e3e3);
+const subTextColor = Color(0xFFdfdfdf);
--- codetest/lib/pages/launches_page.dart
+++ codetest/lib/pages/launches_page.dart
@@ -1,13 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:codetest/config/text.dart';
+import 'package:codetest/config/color.dart';
 
 class LaunchesPage extends StatelessWidget {
   const LaunchesPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Launches Page'),
+    return Scaffold(
+      backgroundColor: bodyColor,
+      appBar: AppBar(
+        backgroundColor: appBarColor,
+        elevation: 4,
+        leading: const Icon(
+          Icons.arrow_back,
+          color: appBarIconColor,
+        ),
+        title: const Text(
+          'All Launches',
+          style: appBarTextStyle,
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.more_vert,
+              color: appBarIconColor,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                // Sorting Button and not full width
+                IconButton(
+                  icon: const Row(
+                    children: [
+                      Icon(Icons.sort, color: subTextActionColor),
+                      SizedBox(width: 8),
+                      Text('Flight number: oldest', style: sortButtonTextStyle),
+                    ],
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: ListView.builder(
+              itemCount: 20,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('Launch ${index + 1}', style: textStyle),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
